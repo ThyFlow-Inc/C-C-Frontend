6604c534b1dbcd216337e1912aa943fab70947ec
Adetayo <tuneymitch@gmail.com>
2020-09-21 11:37:03 +0100

    authpage

---
+++ src/pages/AuthPage.jsx
@@ -0,0 +1,64 @@
+import React, { Component } from 'react'
+
+class AuthPage extends Component {
+  render() {
+    return (
+        <div class="auth">
+          <div>&nbsp;</div>
+          <div class="formpage">
+            <h2 class="form-heading">Deal with verified business Providers</h2>
+            <p class="form-paragraph">
+              We help you choose the best business provider easily
+            </p>
+            <div class="auth-toggle">
+              <a class="auth-login">Login</a>
+              <a class="auth-login">Sign Up</a>
+            </div>
+            <form class="form">
+              <input
+
+                  type="text"
+                  class="form__input"
+                  placeholder="Email"
+              />
+              <input
+                  v-model="password"
+                  type="password"
+                  class="form__input"
+                  placeholder="Password"
+              />
+              <a href="" type="submit" class="form__button">Login</a>
+            </form>
+            <form v-else class="form">
+              <input
+                  v-model="name"
+                  type="text"
+                  class="form__input"
+                  placeholder="Full Name"
+              />
+              <input
+                  v-model="email"
+                  type="text"
+                  class="form__input"
+                  placeholder="Email"
+              />
+              <input
+
+                  type="password"
+                  class="form__input"
+                  placeholder="Password"
+              />
+              <a href="" type="submit" class="form__button">Sign Up</a>
+            </form>
+            <div class="form__foot">
+              <p class="form-paragraph">Or login with</p>
+              <p class="form-social">Google</p>
+              <p class="form-social">Facebook</p>
+              <p class="form-social">Twitter</p>
+            </div>
+          </div>
+        </div>
+    )
+
+  };
+export default AuthPage;
